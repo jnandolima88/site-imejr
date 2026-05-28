93c3fd790ba7a8fe951a627f5964ee7ab1fa0fab
Joel Landim <joellandim@usp.br>
2026-05-27 22:34:08 -0300

    fix: corrigindo rotas

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",   // gera pasta /out com HTML estático
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
