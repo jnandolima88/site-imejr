1ff177c482c32a1c20e22974f9bd9638779c612d
Joel Landim <joellandim@usp.br>
2026-06-10 16:23:15 -0300

    fix: corrigindo fuso horário do contato

--- public/contact.php
+++ public/contact.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+date_default_timezone_set('America/Sao_Paulo');
+
 header('Content-Type: application/json; charset=utf-8');
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
